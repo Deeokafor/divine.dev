90c1f603794b90e65651ba65e8f75ecf9c2d339b
Deeokafor <okafor.udahdivine@gmail.com>
2022-06-14 21:29:41 +0100

    updated about page ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     './assets/script.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        'dee-radius-6': '6px'
+      },
+      colors: {
+        'dee-background-primary': '#0A192F',
+        'dee-background-secondary': '#112240',
+        'dee-text-primary': '#CCD6F6',
+        'dee-text-secondary': '#8892B0',
+        'dee-text-tertiary': '#79F4DB',
+        'dee-text-placeholder': '#C5EAE3'
+      },
+      border: {
+        'dee-border-color-main': '#0A192F'
+      }
+    },
   },
   plugins: [],
 }
