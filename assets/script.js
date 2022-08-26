dc63fca079670948821d76df2e7e12499d19f9d3
Deeokafor <okafor.udahdivine@gmail.com>
2022-08-27 00:02:55 +0100

    added interactivity to mobile navbar

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+const menuOpen = document.querySelector('#menu_open');
+const menuClose = document.querySelector('#menu_close');
+const mobileNavBar = document.querySelector('#mobile_navbar');
+
+//Return checks for elements in DOM
+function isElement(elem) {
+    if (elem) {
+        console.log(`${elem.id} button exists!`)
+    }
+}
+
+isElement(menuOpen);
+isElement(menuClose);
+isElement(mobileNavBar);
+
+menuOpen.addEventListener('click', function () {
+    mobileNavBar.classList.remove('tw-hidden');
+})
+
+menuClose.addEventListener('click', function () {
+    mobileNavBar.classList.add('tw-hidden');
+})
+
+
